874700e184488ad51f933073d71476a624ffa0bb
Tres_zE <rulosan77@gmail.com>
2023-03-03 22:53:38 -0600

    clase03 completada

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const btn = document.querySelector("[data-form-btn]");
-/*se crea una funcion para agregar un evento al boton y que logre captar datos*/
+/*se crea una funcion para agregar un evento al boton y que logre capturar datos*/
 const createTask =  (evento) =>{
     evento.preventDefault();
     const imput = document.querySelector("[data-form-imput]");
-    console.log(imput.value); 
+    const value = imput.value;
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement('li');
+    task.classList.add('card');/*la clase card esta en css*/
+    imput.value = '';
+    /*para mostrar datos en pantalla*/
+    const content = `<div>                       
+        <i class="far fa-check-square icon"></i>
+        <span class="task">${value}</span>
+        </div>
+        <i class="fas fa-trash-alt trashIcon icon"></i>`
+    task.innerHTML = content;
+
+    list.appendChild(task);
+
+    console.log(content); 
 }
 
 console.log(btn);
